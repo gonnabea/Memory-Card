4b39ef7e0d07b42287890a3faf932a49d1cb76da
Choi Jiwon <winning1993@naver.com>
2020-06-03 23:12:09 +0900

    카드 애니메이션

--- script.js
+++ script.js
@@ -2,18 +2,27 @@ const addCardBtn = document.getElementById("addCardBtn");
 const addModal = document.getElementById("addModal"),
 mainArea = document.getElementById("mainArea"),
 modalSubmit = document.getElementById("modalSubmit"),
-closeModalBtn = document.getElementById("closeModalBtn")
+closeModalBtn = document.getElementById("closeModalBtn"),
+cardContent = document.getElementById("cardContent"),
+inputQ = document.getElementById("inputQ"),
+inputA = document.getElementById("inputA"),
+card = document.getElementById("card"),
+addForm = document.getElementById("addForm"),
+riorContent = document.getElementById("riorContent")
 
 function showModal(){
     
     addModal.style.animation = "showWindow 0.5s forwards";
     mainArea.style.animation = "hideWindow 0.5s forwards"
     closeModalBtn.addEventListener("click", closeModal);
-    modalSubmit.addEventListener("click", handleSubmit);
+    addForm.addEventListener("submit", handleSubmit);
 }
 
 function handleSubmit(e){
-    e.preventDefault()
+    e.preventDefault();
+    cardContent.innerHTML = inputA.value;
+    riorContent.innerHTML = inputQ.value
+    closeModal()
 }
 
 function closeModal(){
@@ -21,8 +30,24 @@ function closeModal(){
     mainArea.style.animation = "showWindow 0.5s forwards"
 }
 
+function rotateCard(){
+    card.style.animation = "rotateCard 0.5s forwards";
+    cardContent.style.display = "none"
+    riorContent.style.display = "block"
+    card.addEventListener("click", revertCard);
+}
+
+function revertCard(){
+    card.style.animation = "rotateCard2 0.5s forwards";
+    riorContent.style.display = "none"
+    cardContent.style.display = "block"
+    card.removeEventListener("click", revertCard)
+    card.addEventListener("click", rotateCard)
+}
+
 function init(){
     addCardBtn.addEventListener("click", showModal);
+    card.addEventListener("click", rotateCard)
 }
 
 init();
