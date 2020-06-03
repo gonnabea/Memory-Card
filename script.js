a24fc656a17c8f4a12beddbcebacc7fbc499a6e0
Choi Jiwon <winning1993@naver.com>
2020-06-04 00:17:28 +0900

    카드 추가 구현

--- script.js
+++ script.js
@@ -8,10 +8,21 @@ inputQ = document.getElementById("inputQ"),
 inputA = document.getElementById("inputA"),
 card = document.getElementById("card"),
 addForm = document.getElementById("addForm"),
-riorContent = document.getElementById("riorContent")
+riorContent = document.getElementById("riorContent"),
+page = document.getElementById("page"),
+preCardBtn = document.getElementById("preCardBtn"),
+nextCardBtn = document.getElementById("nextCardBtn")
+
+let frontContents = [];
+let backContents = [];
+let cardIndex = 0;
+let currentIndex = 0;
+
+function handlePage(){
+    page.innerHTML = `${currentIndex} / ${cardIndex}`
+}
 
 function showModal(){
-    
     addModal.style.animation = "showWindow 0.5s forwards";
     mainArea.style.animation = "hideWindow 0.5s forwards"
     closeModalBtn.addEventListener("click", closeModal);
@@ -20,9 +31,17 @@ function showModal(){
 
 function handleSubmit(e){
     e.preventDefault();
-    cardContent.innerHTML = inputA.value;
-    riorContent.innerHTML = inputQ.value
+    frontContents.push(inputA.value);
+    backContents.push(inputQ.value);
+    cardIndex+=1;
+    if(cardIndex === 1){
+        currentIndex +=1;
+        cardContent.innerHTML = frontContents[cardIndex-1];
+        riorContent.innerHTML = backContents[cardIndex-1];
+    }
     closeModal()
+    handlePage()
+    rotateCard()
 }
 
 function closeModal(){
@@ -45,9 +64,30 @@ function revertCard(){
     card.addEventListener("click", rotateCard)
 }
 
+function nextCard(){
+    if(cardIndex > currentIndex){
+        currentIndex +=1;
+        cardContent.innerHTML = frontContents[currentIndex-1];
+        riorContent.innerHTML = backContents[currentIndex-1];
+        handlePage()
+    }
+}
+
+function preCard(){
+    if(currentIndex > 1){
+        currentIndex -=1;
+        cardContent.innerHTML = frontContents[currentIndex-1];
+        riorContent.innerHTML = backContents[currentIndex-1];
+        handlePage()
+    }
+}
+
 function init(){
     addCardBtn.addEventListener("click", showModal);
-    card.addEventListener("click", rotateCard)
+    card.addEventListener("click", rotateCard);
+    handlePage();
+    preCardBtn.addEventListener("click", preCard);
+    nextCardBtn.addEventListener("click", nextCard)
 }
 
 init();
